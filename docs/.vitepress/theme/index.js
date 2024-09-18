ad5e7b085cc5079e529b048feac131f26f59e45c
Sarmaged <3858245+Sarmaged@users.noreply.github.com>
2024-09-18 22:26:15 +0300

    Initial documentation setup

---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,20 @@
+// https://vitepress.dev/guide/custom-theme
+import { h } from 'vue'
+import DefaultTheme from 'vitepress/theme'
+import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
+import highlight from '../components/highlight.vue'
+import actions from '../components/actions.vue'
+
+import './var.css'
+import './style.css'
+
+/** @type {import('vitepress').Theme} */
+export default {
+  ...DefaultTheme,
+  Layout: () => h(DefaultTheme.Layout, null, {}),
+  enhanceApp({ app }) {
+    app.component('highlight', highlight)
+    app.component('actions', actions)
+    enhanceAppWithTabs(app)
+  }
+}
