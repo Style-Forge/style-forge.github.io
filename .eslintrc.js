f0da481533a31ea72f7988e8f56f52f0ae5796ff
Sarmaged <3858245+Sarmaged@users.noreply.github.com>
2025-01-25 15:33:22 +0300

    feat: upgrade packages / linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,15 @@ module.exports = {
   parserOptions: {
     parser: '@babel/eslint-parser',
     requireConfigFile: false,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'eslint:recommended', 'prettier'],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -8,25 +8,39 @@ export default {
     title: 'Style-Forge',
     htmlAttrs: {
       lang: 'en',
-      'data-theme': 'auto'
+      'data-theme': 'auto',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'CSS framework with themes, utilities, flexbox, and grid for creating responsive websites' },
-      { name: 'keywords', content: 'style-forge, CSS framework, modern design, responsive design, utilities, web development, frontend, UI components, CSS grid, CSS flexbox, themes, form validation, custom themes, grid, flex, input, select, switch, textarea, helpers, colors' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'CSS framework with themes, utilities, flexbox, and grid for creating responsive websites',
+      },
+      {
+        name: 'keywords',
+        content:
+          'style-forge, CSS framework, modern design, responsive design, utilities, web development, frontend, UI components, CSS grid, CSS flexbox, themes, form validation, custom themes, grid, flex, input, select, switch, textarea, helpers, colors',
+      },
 
       { name: 'format-detection', content: 'telephone=no' },
 
       { property: 'og:title', content: 'Style-Forge' },
-      { property: 'og:description', content: 'CSS framework with themes, utilities, flexbox, and grid for creating responsive websites' },
+      {
+        property: 'og:description',
+        content: 'CSS framework with themes, utilities, flexbox, and grid for creating responsive websites',
+      },
       { property: 'og:url', content: 'https://style-forge.github.io/' },
       { property: 'og:image', content: '/images/main.png' },
       { property: 'og:type', content: 'website' },
       { name: 'twitter:title', content: 'Style-Forge' },
-      { name: 'twitter:description', content: 'CSS framework with themes, utilities, flexbox, and grid for creating responsive websites' },
+      {
+        name: 'twitter:description',
+        content: 'CSS framework with themes, utilities, flexbox, and grid for creating responsive websites',
+      },
       { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:image', content: '/images/main.png' }
+      { name: 'twitter:image', content: '/images/main.png' },
     ],
     link: [
       { rel: 'apple-touch-icon', size: '180x180', href: '/icons/icon-180x180.png' },
@@ -35,18 +49,22 @@ export default {
       { rel: 'manifest', href: '/site.webmanifest' },
       { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: true },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,200,0,0' }
-    ]
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,200,0,0',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['~/assets/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    { src: '~/plugins/support.js', mode: 'client' }
-  ],
+  plugins: [{ src: '~/plugins/support.js', mode: 'client' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -54,7 +72,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -62,15 +80,16 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    friendlyErrors: false,
     publicPath: '_',
     extractCSS: true,
     postcss: {
       postcssOptions: {
         plugins: {
-          'postcss-preset-env': false
-        }
-      }
-    }
+          'postcss-preset-env': false,
+        },
+      },
+    },
   },
 
   /* /// */
@@ -78,15 +97,15 @@ export default {
   loadingIndicator: false,
 
   router: {
-    mode: 'abstract'
+    mode: 'abstract',
   },
 
   generate: {
     cache: false,
-    fallback: false
+    fallback: false,
   },
 
   server: {
-    port: 5173
-  }
+    port: 5173,
+  },
 }
